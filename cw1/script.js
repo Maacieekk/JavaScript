a742cb464b57caf315fcddac196d9907a4cdf953
Maacieekk <nie1wiem2@gmail.com>
2022-06-28 19:15:58 +0200

    click

--- cw1/script.js
+++ cw1/script.js
@@ -1,12 +1,39 @@
 let ndx = 0;
-Link[
-    "C:\Users\Maciek\Desktop\JavaScript\cw1\Image\image1.jpg",
-    "C:\Users\Maciek\Desktop\JavaScript\cw1\Image\image2.jpg",
-    "C:\Users\Maciek\Desktop\JavaScript\cw1\Image\image3.jpg"
+const data = [
+   { title : "pink lake",
+    src : "image/1.webp" },
+
+  {  title : "drugie",
+    src : "image/2.webp"},
+
+   { title : "trzecie",
+    src : "image/3.webp"}
 ]
 
+function changeHTMLinfo(ndx)
+{
+    console.log(data[ndx].title,data[ndx].src);
+    const obraz = document.querySelector('.obraz')
+    obraz.src = data[ndx].src;
+    const h2 = document.querySelector('h2')
+    h2.innerText = data[ndx].title;
+}
 
+// const zdjęcia = document.createElement("div")
+//  newdom.innerHTML = "jakiś tekst"
+//  console.log(newdom)
+const left = document.querySelector('.lefftArr')
+left.onclick = function(){
+    console.log("click")
+ndx = ndx-1;
 
-const zdjęcia = document.createElement("div")
- newdom.innerHTML = "jakiś tekst"
- console.log(newdom)
+changeHTMLinfo(ndx);
+}
+
+const right = document.querySelector('.rightArr')
+right.onclick = function(){
+    console.log("click")
+ndx = ndx+1;
+
+changeHTMLinfo(ndx);
+}
