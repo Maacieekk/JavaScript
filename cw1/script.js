1ee306cc5934c97763df41ae8ca64811b948132d
Maacieekk <nie1wiem2@gmail.com>
2022-06-29 17:26:45 +0200

    ok

--- cw1/script.js
+++ cw1/script.js
@@ -44,10 +44,17 @@ else ndx = 0;
 changeHTMLinfo(ndx);
 console.log(ndx)
 }
-const div1 = document.querySelector('.div1')
-div1.onclick = function(){
-ndx = div1.innerText
-changeHTMLinfo(ndx);
-console.log(ndx+"   navigacja")
+// const div1 = document.querySelector('.div1')
+// div1.onclick = function(){
+// ndx = div1.innerText - 1;
+// changeHTMLinfo(ndx);
+// console.log(ndx+"   navigacja")
+
+// }
+function mojaFunkcja(ndx)
+{
+    
+    console.log(ndx+"dziala")
+    changeHTMLinfo(ndx);
 
 }
